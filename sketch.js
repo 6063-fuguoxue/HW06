8c1d4121d61a51d80795b016c288f4e890258285
Fuguo Xue <fx2100@nyu.edu>
2023-10-17 23:14:53 -0400

    Added comments

--- sketch.js
+++ sketch.js
@@ -1,6 +1,5 @@
-// let s0 = "Are you blind when you're born? ";
-// let s1 = "Can you see in the dark? "
-// let s = "Can you look at a king? \n Would you sit on his throne? "
+// Visual Musical: Cats
+// Prologue: Jellicle Songs for Jellicle Cats lyrics
 
 let lines;
 let lineIndex = 0;
@@ -9,15 +8,15 @@ let words;
 let wordIndex;
 let currentWordList;
 let currentLinePart;
-let separator = " ";
+let separator = " "; // Separator for Part 1: separate lines into words for animation
 let fr = 4; // Set up the framerate for speed control
-let bgColorList = [0, 0, "purple", "Lavender", 255, 0]; 
-let textColorList = [100, 255, "yellow", "FireBrick", "red", "red"];
+let bgColorList = [0, 0, "purple", "Lavender", 255, 0]; // Background colors for Part 1
+let textColorList = [100, 255, "yellow", "FireBrick", "red", "red"]; // Text colors for Part 1
 let positionSeed = 100;
 
+// Part 2 lyrics lines
 class StylizedLine {
   constructor(_y, _v, _tsize, _line, _color) {
-    
     this.y = _y;
     this.hAlign = CENTER;
     this.vAling = _v;
@@ -34,6 +33,7 @@ class StylizedLine {
   }
 }
 
+// Load the lyrics in .txt file
 function preload() {
   lines = loadStrings("./lyrics.txt");
 }
@@ -70,31 +70,34 @@ function printLine1(bgColor, textColor, currentLine) {
 
 function draw() {
   currentLine = lines[lineIndex];
+  // Part 1 of Cats musical. Different background color and text color for every line. Click to see the next line. 
   if (lineIndex < 6) {
     textSize(40);
-    textAlign(CENTER, CENTER);
+    textAlign(CENTER, CENTER); // All the lines aligned to the center of the screen
     printLine1(bgColorList[lineIndex], textColorList[lineIndex], currentLine);
   }
-  else {
+  else { // Part 2 of Cats 
+    // Since the lyrics form groups of 3, we can let them animate in groups
     let line1, line2, line3;
-    // Print every 3 lines one by one on the same canvas
-    if (frameCount%2 == 0) { // Slow down the 2nd part of animation
-      if (lineIndex % 3 == 0) {
+    
+    if (frameCount%2 == 0) { // Slow down the 2nd part of animation; update the canvas once for every two frames
+      // Print groups of 3 lines one by one
+      if (lineIndex % 3 == 0) { // Step 1: print the 1st line in group
         background(255);
         line1 = currentLine;
         new StylizedLine(height/4, TOP, 60, line1, "OrangeRed").draw();
         lineIndex += 1;
-      } else if (lineIndex % 3 == 1) {
+      } else if (lineIndex % 3 == 1) { // Step 2: print the 1st and 2nd lines in group
         background(0);
         line2 = currentLine;
         line1 = lines[lineIndex - 1];
         new StylizedLine(height/4, TOP, 60, line1, "OrangeRed").draw();
         new StylizedLine(height/2, CENTER, 40, line2, 255).draw();
         lineIndex += 1;
-      } else {
+      } else { // Step 3: print the 1st and 3rd lines in group, as they both begin with "Jellicles"
         background(255);
         line3 = currentLine;
-        line2 = lines[lineIndex - 1];
+        // line2 = lines[lineIndex - 1];
         line1 = lines[lineIndex - 2];
         new StylizedLine(height/4, TOP, 60, line1, "OrangeRed").draw();
         // new StylizedLine(height/2, CENTER, 40, line2, 150).draw();
